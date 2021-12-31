170bdeb68a68c1829c7a4b5392c0374b19a0c928
Jake Johnson <Jakejohnson21@rocketmail.com>
2021-12-30 23:19:11 -0600

    implement open /close popup

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-let;
+let edit = document.querySelector(".edit");
+let editIcon = edit.querySelector(".edit__close-icon");
+let editPopup = document.querySelector(".profile__edit");
+
+function tog() {
+  if (edit.style.display === "none") {
+    edit.style.display = "block";
+  } else {
+    edit.style.display = "none";
+  }
+}
+
+editPopup.addEventListener("click", tog);
+
+editIcon.addEventListener("click", tog);
+
+let formElement = edit;
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+}
+let inputName = document.querySelector(".edit__name");
+let inputTitle = document.querySelector(".edit__title");
