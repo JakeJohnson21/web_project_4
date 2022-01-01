d82f6c298184b2ea69e54c5e79983f5538679fb8
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-01-01 16:37:08 -0600

    javascript midpoint

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let edit = document.querySelector(".edit");
 let editIcon = edit.querySelector(".edit__close-icon");
 let editPopup = document.querySelector(".profile__edit");
+let personName = document.querySelector(".profile__main_name");
+let personTitle = document.querySelector(".profile__text_explorer");
+let nameInput = document.querySelector(".edit__name");
+let titleInput = document.querySelector(".edit__title");
+let save = document.querySelector(".save__input");
 
 function tog() {
   if (edit.style.display === "none") {
@@ -11,12 +16,20 @@ function tog() {
 }
 
 editPopup.addEventListener("click", tog);
-
 editIcon.addEventListener("click", tog);
 
-let formElement = edit;
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
+function togglePopupVisibility() {
+  if (!edit.classList.contains(".popup__is_opened")) {
+    nameInput.value = personName.textContent;
+    titleInput.value = personTitle.textContent;
+  }
+
+  edit.classList.toggle("popup__is_opened");
 }
-let inputName = document.querySelector(".edit__name");
-let inputTitle = document.querySelector(".edit__title");
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+
+  personName.textContent = nameInput.value;
+  personTitle.textContent = titleInput.value;
+}
