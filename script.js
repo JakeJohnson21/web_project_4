dd8883a87c15831c5d09a00d37a1657d9820fa1b
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-01-01 20:26:22 -0600

    struggling with JS

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+//popup box
 let edit = document.querySelector(".edit");
-let editIcon = edit.querySelector(".edit__close-icon");
+//the button on the main page to open the edit popup dialogue box
 let editPopup = document.querySelector(".profile__edit");
+//the X to close out the popup
+let editIcon = edit.querySelector(".edit__close-icon");
+// The end point for the persons name
 let personName = document.querySelector(".profile__main_name");
+// The end point for explorer
 let personTitle = document.querySelector(".profile__text_explorer");
+//the name input field
 let nameInput = document.querySelector(".edit__name");
+//the title (explorer) input field
 let titleInput = document.querySelector(".edit__title");
-let save = document.querySelector(".save__input");
-
+// the SAVE button
+let save = document.querySelector(".edit__save");
+//form edit box
+let editBox = document.querySelector(".edit__box");
 function tog() {
   if (edit.style.display === "none") {
     edit.style.display = "block";
@@ -14,22 +23,18 @@ function tog() {
     edit.style.display = "none";
   }
 }
-
 editPopup.addEventListener("click", tog);
 editIcon.addEventListener("click", tog);
 
-function togglePopupVisibility() {
-  if (!edit.classList.contains(".popup__is_opened")) {
-    nameInput.value = personName.textContent;
-    titleInput.value = personTitle.textContent;
-  }
-
-  edit.classList.toggle("popup__is_opened");
+function toggleModalVisibility() {
+  console.log((titleInput.value = personTitle.textContent));
+  nameInput.value = personName.textContent;
 }
 
-function formSubmitHandler(e) {
-  e.preventDefault();
-
-  personName.textContent = nameInput.value;
+function formSubmitHandler(evt) {
   personTitle.textContent = titleInput.value;
+  personName.textContent = nameInput.value;
+
+  evt.preventDefault();
 }
+save.addEventListener("submit", formSubmitHandler);
