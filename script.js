9ac968b50f8ff6445f15c393bef6cd6a8d7d73ec
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-01-15 11:12:31 -0600

    help

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+/////////////////
+//  Declarations --------
+/////////////////
+
+const initialCards = [
+  {
+    name: "Milwaukee, Wisconsin",
+    link: "images/milwaukee.jpg",
+  },
+  {
+    name: "Chicago, Illinois",
+    link: "images/chicago.jpg",
+  },
+  {
+    name: "Waikiki Beach O'ahu",
+    link: "images/waikiki.jpg",
+  },
+  {
+    name: "Sydney Harbour, Australia",
+    link: "images/sydney.jpg",
+  },
+  {
+    name: "London, England",
+    link: "images/london.jpg",
+  },
+  {
+    name: "Dubai",
+    link: "images/dubai.jpg",
+  },
+];
+
+// Modal-----------
+
 //popup box
-let edit = document.querySelectorAll(".modal");
-//the button on the main page to open the edit popup dialogue box
-let editOpenPopup = document.querySelector(".profile__edit-button");
-//the X to close out the popup
-let editCloseIcon = document.querySelectorAll(".modal__close-button");
-// The end point for the persons name
-let personName = document.querySelector(".profile__title-name");
-// The end point for explorer
-let personTitle = document.querySelector(".profile__text-job");
+let edit = document.querySelector(".modal");
 //the name input field
 let nameInput = document.querySelector(".modal__input_profile_name");
 //the title (explorer) input field
@@ -16,16 +41,41 @@ let titleInput = document.querySelector(".modal__input_profile_title");
 let save = document.querySelector(".modal__save");
 //form edit box
 let editBox = document.querySelector(".modal__box");
-//add button
-let addOpenPopup = document.querySelector(".profile__add");
 //new place main modal
-let modalPlace = document.querySelector(".modal__place");
+let modalPlace = edit.querySelector(".modal__place");
 //the photo title input field
 let photoTitleInput = document.querySelector(".modal__input_image_title");
 //the photo source input field
 let photoInput = document.querySelector(".modal__input_image_link");
+
+// Buttons and other DOM elements ----------
+
+//the X to close out the popup
+let editCloseIcon = document.querySelector(".modal__close-button");
+//the button on the main page to open the edit popup dialogue box
+let editOpenPopup = document.querySelector(".profile__edit-button");
+//add button
+let addOpenPopup = document.querySelector(".profile__add");
+// The end point for the persons name
+let personName = document.querySelector(".profile__title-name");
+// The end point for explorer
+let personTitle = document.querySelector(".profile__text-job");
 //The gallery image place for input
-let galleryImage = document.querySelector;
+let galleryImage = document.querySelector(".card__image");
+
+const placeList = document.querySelector(".card");
+
+////////
+// TEMPLATES------
+////////////////
+
+let cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card__item");
+
+//////////////
+//  Functions--------
+//////////////
 
 function openPopup() {
   edit.classList.toggle("modal_none");
@@ -36,7 +86,11 @@ function closePopup() {
   edit.classList.toggle("modal_none");
 }
 
-function photoSubmitHandler(evt) {}
+document.querySelectorAll(".gallery__like-button").forEach((element) =>
+  element.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("gallery__like-button_active");
+  })
+);
 
 function formSubmitHandler(evt) {
   personTitle.textContent = titleInput.value;
@@ -45,21 +99,25 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-// function ifFormIsEmpty() {
-//   if (nameInput.value.length === 0) {
-//     personName.textContent = "Jacques Cousteau";
-//   }
-//   if (titleInput.value.length === 0) {
-//     personTitle.textContent = "Explorer";
-//   }
-// }
+//////////////////
+// Event Handlers --------
+//////////////////
+
 addOpenPopup.addEventListener("click", function () {
-  edit.classList.toggle("modal_none");
+  modalPlace.classList.toggle("modal_none");
 });
 editOpenPopup.addEventListener("click", openPopup);
 editCloseIcon.addEventListener("click", closePopup);
 editBox.addEventListener("submit", formSubmitHandler);
 
+///// Actions ------
+
+initialCards.forEach(function (card) {
+  //clone the template
+  // query name element
+  //query image element
+  // add event listener
+});
 // function toggleFormVisibility() {
 //   if (edit.style.display === "none") {
 //     edit.style.display = "block";
@@ -79,3 +137,30 @@ editBox.addEventListener("submit", formSubmitHandler);
 // }
 // editBox.addEventListener("submit", formSubmitHandler);
 // save.addEventListener("click", toggleFormVisibility);
+
+// function addCard(cardTitle, cardSource) {
+//   //get the card "template"
+//   const cardTemplate = document.querySelector("#card-template").content;
+//   //get the elements of card "template"
+//   const cardElement = cardTemplate.querySelector("card").cloneNode(true);
+//   //get the title element
+//   cardElement.querySelector(".card__title").textContent = cardTitle;
+//   //get the image src element
+//   cardElement.querySelector(".card__image").src = cardSource;
+//   // append card item to the gallery
+//   cardContainer.append(cardContainer);
+//   cardElement
+//     .querySelector(".gallery__like-button")
+//     .addEventListener("click", function (evt) {
+//       console.log(evt);
+//     });
+// }
+
+// function ifFormIsEmpty() {
+//   if (nameInput.value.length === 0) {
+//     personName.textContent = "Jacques Cousteau";
+//   }
+//   if (titleInput.value.length === 0) {
+//     personTitle.textContent = "Explorer";
+//   }
+// }
